1cee548b031593cf831e14f1cfca36ffa57a450e
U23Rodin <rodin.velichkov.u23@learn.telerikacademy.com>
2024-01-24 17:27:02 +0200

    Changes to terraform-backend: - Parameterize main.tf by using variables - Add S3 encryption, public access block, versioning - Add dynamodb encryption

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "jira-project-state"
+  bucket = var.s3_bucket_name
 
   lifecycle {
     prevent_destroy = true
@@ -9,17 +9,59 @@ resource "aws_s3_bucket" "terraform_state" {
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
-    status = "Enabled"
+    status = var.s3_versioning_configuration
   }
 }
 
+resource "aws_s3_bucket_logging" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  target_bucket = aws_s3_bucket.terraform_state.id
+  target_prefix = var.s3_logging_target_prefix
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.terraform_state.arn
+      sse_algorithm     = var.s3_encryption_sse_algorithm
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = var.s3_block_public_acl
+  block_public_policy     = var.s3_block_public_policy
+  ignore_public_acls      = var.s3_ignore_public_acls
+  restrict_public_buckets = var.s3_restrict_public_buckets
+}
+
+resource "aws_kms_key" "terraform_state" {
+  description             = "KMS key for DynamoDB table encryption"
+  deletion_window_in_days = var.kms_key_deletion_period
+}
+
+
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+  name         = var.dynamodb_table_name
+  billing_mode = var.dynamodb_table_billing_mode
+  hash_key     = var.dynamodb_table_hash_key
+
+  point_in_time_recovery {
+    enabled = var.dynamodb_table_recovery
+  }
+
+  server_side_encryption {
+    enabled     = var.dynamodb_table_encryption
+    kms_key_arn = aws_kms_key.terraform_state.arn
+  }
 
   attribute {
-    name = "LockID"
-    type = "S"
+    name = var.dynamodb_table_hash_key
+    type = var.dynamodb_table_lockId_type
   }
 }
---
+++ terraform-backend/variables.tf
@@ -0,0 +1,89 @@
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "jira-project-state"
+}
+
+variable "s3_versioning_configuration" {
+  description = "Whether the S3 bucket versioning should be enabled"
+  type        = string
+  default     = "Enabled"
+}
+
+variable "s3_logging_target_prefix" {
+  description = "The target prefix/path of the logs"
+  type        = string
+  default     = "log/"
+}
+
+variable "s3_encryption_sse_algorithm" {
+  description = "The sse algorithm used for server side encryption"
+  type        = string
+  default     = "aws:kms"
+}
+
+variable "s3_block_public_acl" {
+  description = "Whether public acls should be blocked"
+  type        = bool
+  default     = true
+}
+
+variable "s3_block_public_policy" {
+  description = "Whether public policies should be blocked"
+  type        = bool
+  default     = true
+}
+
+variable "s3_ignore_public_acls" {
+  description = "Whether public policies should be ignored"
+  type        = bool
+  default     = true
+}
+
+variable "s3_restrict_public_buckets" {
+  description = "Whether public policies should be blocked"
+  type        = bool
+  default     = true
+}
+
+variable "kms_key_deletion_period" {
+  description = "The deletion window in days for the kms key"
+  type        = number
+  default     = 7
+}
+
+variable "dynamodb_table_name" {
+  description = "The name of the dynamodb table"
+  type        = string
+  default     = "terraform-state-locking"
+}
+
+variable "dynamodb_table_billing_mode" {
+  description = "The billing mode of the dynamodb table"
+  type        = string
+  default     = "PAY_PER_REQUEST"
+}
+
+variable "dynamodb_table_hash_key" {
+  description = "The hash key for the dynamodb table"
+  type        = string
+  default     = "LockID"
+}
+
+variable "dynamodb_table_recovery" {
+  description = "Whether the dynamodb table has point in time recovery enabled"
+  type        = bool
+  default     = true
+}
+
+variable "dynamodb_table_encryption" {
+  description = "Whether the dynamodb table has server side encryption"
+  type        = bool
+  default     = true
+}
+
+variable "dynamodb_table_lockId_type" {
+  description = "The type of the LockID"
+  type        = string
+  default     = "S"
+}
